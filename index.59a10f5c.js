325de7f820833bd3563309a4cef890eb4039b60c
Victor Komara <vitioklss@gmail.com>
2024-08-10 17:27:04 +0300

    make build

---
+++ index.59a10f5c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.left="".concat(Number.parseInt(getComputedStyle(t).width)/2-Number.parseInt(getComputedStyle(e).width)/2,"px"),e.style.top="".concat(Number.parseInt(getComputedStyle(t).height)/2-Number.parseInt(getComputedStyle(e).height)/2,"px");
+//# sourceMappingURL=index.59a10f5c.js.map
